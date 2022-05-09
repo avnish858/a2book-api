48437216ecfba1681886eaa41b1df0847648c55d
avnish <avnish.dwivedi@oncehub.com>
2022-05-09 20:13:07 +0530

    afaf

---
+++ app/models/user.rb
@@ -0,0 +1,42 @@
+class User < ApplicationRecord
+  has_many :posts
+  has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :notifications, dependent: :destroy
+  has_many :friend_sent, class_name: 'Friendship',
+                         foreign_key: 'sent_by_id',
+                         inverse_of: 'sent_by',
+                         dependent: :destroy
+  has_many :friend_request, class_name: 'Friendship',
+                        foreign_key: 'sent_to_id',
+                        inverse_of: 'sent_to',
+                        dependent: :destroy
+  has_many :friends, -> { merge(Friendship.friends) },
+            through: :friend_sent, source: :sent_to
+  has_many :pending_requests, -> { merge(Friendship.not_friends) },
+            through: :friend_sent, source: :sent_to
+  has_many :received_requests, -> { merge(Friendship.not_friends) },
+            through: :friend_request, source: :sent_by
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  def full_name
+    "#{fname} #{lname}"
+  end
+  
+  # Returns all posts from this user's friends and self
+  def friends_and_own_posts
+    myfriends = friends
+    our_posts = []
+    myfriends.each do |f|
+      f.posts.each do |p|
+        our_posts << p
+      end
+    end
+    posts.each do |p|
+      our_posts << p
+    end
+    our_posts
+  end
+end
